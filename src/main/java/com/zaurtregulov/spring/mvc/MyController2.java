a0baad489b8bf32d4af1bdf43d379dd292644ede
OlegChumin <tschumin.oleg@gmail.com>
2023-06-03 14:39:20 +0300

    Add new files

--- src/main/java/com/zaurtregulov/spring/mvc/MyController2.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController2.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
@@ -35,6 +36,7 @@ public class MyController2 {
     }
 
     @RequestMapping("/askDetails") //URL
+//    @RequestMapping(path="/askDetails", method = RequestMethod.GET) //URL
     public String askEmplyeeDetails(Model model) {
 
 //        Employee employee = new Employee();
